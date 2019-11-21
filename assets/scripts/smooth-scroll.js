8043933ffbb31965acd657c46dbf004675da2a00
Chris Durheim <christopherdurheim@gmail.com>
2019-11-20 23:13:26 -0600

    redesign - woohoo

---
+++ assets/scripts/smooth-scroll.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  function smoothScroll(id) {
+    document.querySelector(id).scrollIntoView({
+      block: "start",
+      inline: "nearest",
+      behavior: 'smooth'
+    });
+  }
+
+  scrollLinks = document.querySelectorAll('a.smooth-scroller');
+
+  for (let i = 0; i < scrollLinks.length; i += 1) {
+    scrollLinks[i].addEventListener('click', (ele) => {
+      ele.preventDefault();
+      smoothScroll(ele.target.hash);
+    });
+  }
+});
